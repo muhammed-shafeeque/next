067f8f4165b212634b1fea317dfd3184b741d416
shafeeqAGTS <muhammad.shafeek@agtstech.com>
2022-07-28 22:45:08 +0530

    delete old stracture for new stracture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/view/dashboard/dashboard.component';
-import { ToDoComponent } from './dashboard/view/to-do/to-do.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    ToDoComponent
   ],
   imports: [
     BrowserModule,
